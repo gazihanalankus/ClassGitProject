fefa8f74f0a4d9226b55330619d6e6c3ed3a352e
gazihanalankus <gazihan.alankus@ieu.edu.tr>
2014-03-21 16:20:27 +0200

    added functions for everyone

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,10 +2,62 @@
 using System.Collections;
 
 public class NewBehaviourScript : MonoBehaviour {
+	void ayberkyatagan ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void emressio ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void burakalakuss ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void ihsankcsbn ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void andactemel ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void HuseyinUtkuASLAN ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void keremdincer ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void lozgur ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+	void alpersilistre ()
+	{
+		throw new System.NotImplementedException ();
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		ayberkyatagan ();
+		emressio();
+		burakalakuss();
+		ihsankcsbn();
+		andactemel();
+		HuseyinUtkuASLAN();
+		keremdincer();
+		lozgur();
+		alpersilistre();
 	}
 	
 	// Update is called once per frame
